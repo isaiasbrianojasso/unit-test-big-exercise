0ecab25c5c348de06ac63757be89c51d23405e7f
isaiasbrianojasso <isaias.briano@gmail.com>
2020-11-13 12:58:40 -0600

    unitest

--- src/main/java/com/nearsoft/training/library/App.java
+++ src/main/java/com/nearsoft/training/library/App.java
@@ -1,5 +1,4 @@
 package com.nearsoft.training.library;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
